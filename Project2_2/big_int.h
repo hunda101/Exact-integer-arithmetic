91fe7e1d640a7df086ef0eefd70b4ad8ad658207
hunda101 <47692698+hunda101@users.noreply.github.com>
2023-11-16 22:39:51 +0200

    added all methods

---
+++ Project2_2/big_int.h
@@ -0,0 +1,12 @@
+//
+//  big_int.h
+//  Project2_2
+//
+//  Created by Mark Khomenko on 08.11.2023.
+//
+
+#ifndef big_int_h
+#define big_int_h
+
+
+#endif /* big_int_h */
---
+++ Project2_2/big_int_function_def.h
@@ -0,0 +1,12 @@
+//
+//  big_int_function_def.h
+//  Project2_2
+//
+//  Created by Mark Khomenko on 08.11.2023.
+//
+
+#ifndef big_int_function_def_h
+#define big_int_function_def_h
+
+
+#endif /* big_int_function_def_h */
